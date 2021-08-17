4d3834a21135ad069e5da144cf39b9b745aa3a10
matanavneri <matan.av@gmail.com>
2021-08-17 09:46:34 -0600

    feat: support dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createTheme, ThemeProvider } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+
 import './App.css'
 import EmailInputs from './components/EmailInputs'
 import EmailPreview from './components/EmailPreview'
@@ -13,17 +15,32 @@ const AppContainer = styled.div`
   height: 100vh;
   padding: 5rem;
   grid-gap: 5rem;
+  background: ${props => props.theme.palette.background.default};
 `
 
 const App: React.FC = () => {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
   return (
-    <StylesProvider injectFirst>
-      <AppContainer>
-        <EmailPreview />
-        <EmailInputs />
-        <EmailStyles />
-      </AppContainer>
-    </StylesProvider>
+    <ThemeProvider theme={theme}>
+      <StylesProvider injectFirst>
+        <AppContainer theme={theme}>
+          <EmailPreview />
+          <EmailInputs />
+          <EmailStyles />
+        </AppContainer>
+      </StylesProvider>
+    </ThemeProvider>
   )
 }
 
